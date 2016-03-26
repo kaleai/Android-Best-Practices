5b6e70e490f956a51b86ab1e1a53b13ddd77e725
kale <kai.jin@duitang.com>
2016-03-26 10:58:12 +0800

    修改了之前log的文章，整理了readme

--- app/src/main/java/kale/toolbox/adapter/bean.java
+++
@@ -1,9 +0,0 @@
-package kale.toolbox.adapter;
-
-/**
- * @author Jack Tony
- * @date 2015/9/17
- */
-public class bean {
-
-}
--- lib/src/main/java/kale/callback/Callback.java
+++
@@ -1,9 +0,0 @@
-package kale.callback;
-
-/**
- * @author Jack Tony
- * @date 2015/8/3
- */
-public interface Callback {
-
-}
